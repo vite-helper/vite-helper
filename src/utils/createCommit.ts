51727586050ad7c4bfb8e6637fe7d3e4bc916ca5
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-05-27 08:54:38 -0300

    fix: silent shell imports

--- src/utils/createCommit.ts
+++ src/utils/createCommit.ts
@@ -1,5 +1,5 @@
 import shell from "shelljs";
-import { silentExec } from "./shell";
+import { silentExec } from "./silentExec";
 
 export const createCommit = async (commitName: string) => {
   silentExec(`git commit -m "${commitName}"`);
