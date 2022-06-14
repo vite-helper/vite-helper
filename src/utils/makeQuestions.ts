59b712b4ed55d9b3b71f74e7fb14638c835ae0d6
Lucas Estevam <dev.lucasestevam@gmail.com>
2022-06-14 12:11:08 -0300

    refactor: removing not scratch project option

--- src/utils/makeQuestions.ts
+++ src/utils/makeQuestions.ts
@@ -1,57 +1,40 @@
 import inquirer from "inquirer";
 
 interface IQuestionsData {
-  isScratch: boolean;
   tools: string[];
-  projectDetails?: {
+  projectDetails: {
     projectName: string;
-    packageManager: string;
     isTypescript: boolean;
   };
 }
 
 export const makeQuestions = async (): Promise<IQuestionsData> => {
-  const { isScratch } = await inquirer.prompt({
-    type: "confirm",
-    name: "isScratch",
-    message: "Do you want to create a project from scratch?",
-  });
-
-  let projectDetails = null;
-  if (isScratch) {
-    projectDetails = await inquirer.prompt([
-      {
-        type: "input",
-        name: "projectName",
-        message: "What is the name of your project?",
-        default: "my-project",
-      },
-      {
-        type: "confirm",
-        name: "isTypescript",
-        message: "Do you want to use typescript?",
-        default: false,
-      },
-      {
-        type: "list",
-        name: "packageManager",
-        message: "Which package manager do you want to use?",
-        choices: ["npm", "yarn"],
-      },
-    ]);
-  }
+  const projectDetails = await inquirer.prompt([
+    {
+      type: "input",
+      name: "projectName",
+      message: "What is the name of your project?",
+      default: "my-project",
+    },
+    {
+      type: "confirm",
+      name: "isTypescript",
+      message: "Do you want to use typescript?",
+      default: false,
+    },
+  ]);
 
   const { tools } = await inquirer.prompt({
     type: "checkbox",
     name: "tools",
     message: "Which tools do you want to use?",
     choices: [
-      "ESlint, Prettier and Editorconfig",
-      "Vite svgr",
-      "React Router Dom",
-      "Tailwind",
+      "🧑‍💻 | ESlint, Prettier and Editorconfig",
+      "🧑‍💻 | Vite svgr",
+      "✨ | React Router Dom",
+      "🎨 | Tailwind",
     ],
   });
 
-  return { isScratch, tools, projectDetails };
+  return { tools, projectDetails };
 };
