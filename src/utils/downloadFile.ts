0241de208a0b7574912325e4bd7431f47db0bab2
Lucas Estevam <dev.lucasestevam@gmail.com>
2022-06-23 18:08:52 -0300

    feat: dotenv

--- src/utils/downloadFile.ts
+++ src/utils/downloadFile.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { DownloaderHelper } from "node-downloader-helper";
 
 import { errLog } from "./logs";
@@ -5,8 +6,11 @@ import { errLog } from "./logs";
 export const downloadFile = async (assetName: string, path: string) => {
   console.log(`Downloading: ${assetName}`);
 
-  const baseUrl =
-    "https://raw.githubusercontent.com/tl-lucasestevam/vite-helper/develop/assets/";
+  const username = process.env.GITHUB_USERNAME ?? "vite-helper";
+  const repo = process.env.GITHUB_REPO ?? "vite-helper";
+  const branch = process.env.GITHUB_BRANCH ?? "main";
+
+  const baseUrl = `https://raw.githubusercontent.com/${username}/${repo}/${branch}/assets/`;
 
   const dl = new DownloaderHelper(
     `${baseUrl}${assetName}`,
