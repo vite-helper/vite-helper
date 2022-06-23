cfb3623279cc8ec477f7a64cbd23f7e53429a2e3
Lucas Estevam <dev.lucasestevam@gmail.com>
2022-06-23 04:02:18 -0300

    fix: vitest structure

--- assets/vitest/js/vitest.config.js
+++ assets/vitest/js/vitest.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/setup-test.js"],
+    setupFiles: ["./src/.vitest/setup.ts"],
   },
 });
