83aa24a9c8c7d4052e89594fedac5d615b5e87ac
Lucas Estevam <dev.lucasestevam@gmail.com>
2022-06-24 08:16:14 -0300

    fix: asyncronity problem with downloadFile

--- src/utils/downloadFile.ts
+++ src/utils/downloadFile.ts
@@ -3,23 +3,34 @@ import { DownloaderHelper } from "node-downloader-helper";
 
 import { errLog } from "./logs";
 
-export const downloadFile = async (assetPath: string, destinationPath = "") => {
+export const downloadFile = async (
+  assetPath: string,
+  destinationPath = "",
+): Promise<void> => {
   const username = process.env.GITHUB_USERNAME ?? "vite-helper";
   const repo = process.env.GITHUB_REPO ?? "vite-helper";
   const branch = process.env.GITHUB_BRANCH ?? "main";
 
   const baseUrl = `https://raw.githubusercontent.com/${username}/${repo}/${branch}/templates/`;
 
-  const dl = new DownloaderHelper(
-    `${baseUrl}${assetPath}`,
-    `${process.cwd()}${destinationPath}`,
-  );
+  return new Promise((resolve, reject) => {
+    const dl = new DownloaderHelper(
+      `${baseUrl}${assetPath}`,
+      `${process.cwd()}${destinationPath}`,
+    );
 
-  await dl.on("error", err =>
-    errLog(assetPath + String(err).replace("Error:", ", ")),
-  );
-  await dl
-    .start()
-    .then(() => console.log(`Downloading: ${assetPath}`))
-    .catch(err => errLog(assetPath + String(err).replace("Error:", ", ")));
+    console.log(`Downloading: ${assetPath}`);
+
+    dl.on("end", () => resolve());
+
+    dl.start().catch(err => {
+      errLog(assetPath + String(err).replace("Error:", ", "));
+      reject(err);
+    });
+
+    dl.on("error", err => {
+      errLog(assetPath + String(err).replace("Error:", ", "));
+      reject(err);
+    });
+  });
 };
