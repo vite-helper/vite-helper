24c2f8a5287015b40f23d8ddbf670fcf6ee6fe13
wesleyara <wes1gom@gmail.com>
2022-11-22 00:48:22 -0300

    feat: adding create file in fileRequest

--- src/utils/fileRequest.ts
+++ src/utils/fileRequest.ts
@@ -1,19 +1,29 @@
 import axios from "axios";
+import fs from "fs";
 
 import { IGithubFile } from "../@types";
 
 interface IFileRequestProps {
   path: string;
   branch: string;
+  file?: string;
 }
 
-export const fileRequest = async ({ path, branch }: IFileRequestProps) => {
+export const fileRequest = async ({
+  path,
+  branch,
+  file,
+}: IFileRequestProps) => {
   const response = await axios.get(
     `https://api.github.com/repos/vite-helper/vite-helper/contents/${path}?ref=${branch}`,
   );
 
   const data = (await response.data) as IGithubFile;
   const content = Buffer.from(data.content, "base64").toString("utf-8");
+
+  if (file) {
+    return fs.writeFileSync(file, content);
+  }
 
   return content;
 };
